bfd1b6bc697c297fb401ac99e570d320c8891ea8
LukasMandik <swipe47@gmail.com>
2023-10-24 20:10:55 +0200

    Add allowed host

--- georeal/settings.py
+++ georeal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d^jg3*hfr)z(pit$s6*=opsc)pjpt2uw@er(t%_zy&5yalhtmt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["geo-real.onrender.com"]
 
 
 # Application definition
